6846b386e2b4061a02734e68cd7fa17214ccaecf
sergey <sergey@kinescope.io>
2025-02-19 12:53:49 +0400

    fix test

--- cypress/e2e/page_objects/iframe.js
+++ cypress/e2e/page_objects/iframe.js
@@ -3,8 +3,14 @@ class iFramePage {
         cy.visit('https://sergesiluyanov.github.io/embedHabrTest/');
     }
 
-    getIframe() {
-        cy.get('iframe');
+    iFramePlayButton() {
+        cy.get('iframe[src="https://rutube.ru/play/embed/bed49f6658d1b7755b08ae45b5e7d002"]') // Здесь указываем селектор iframe
+        .its('0.contentDocument.body') // Получаем доступ к содержимому iframe
+        .should('not.be.empty') // Проверяем, что содержимое не пустое
+        .then(cy.wrap) // Оборачиваем содержимое, чтобы использовать Cypress команды
+        .find('[data-testid="ui-play"]') // Находим кнопку по атрибуту data-testid
+        .click(); // Кликаем по кнопке
+      
     }
 }
 
--- cypress/e2e/videoIsPlaying.cy.js
+++ cypress/e2e/videoIsPlaying.cy.js
@@ -1,7 +1,18 @@
 import iFramePage from './page_objects/iframe.js';
 
 describe('verify video is playing', () => {
+
+  before(() => {
+    // вызов функции на блокировку запросов
+    cy.blockAllRequests();
+  });
+
     it('clicks play button', () => {
       iFramePage.visit();
+
+      iFramePage.iFramePlayButton();
+
+
+
     })
   })
